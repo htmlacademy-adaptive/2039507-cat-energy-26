127686ac4268ab29c8601fb2354db70762b8bb9f
Igor Bolduev <101598982+igorbolduev@users.noreply.github.com>
2023-06-19 15:42:14 +0400

    добавляет JS-script к бургег-меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,9 @@
-let mainNav = document.querySelector(".main-nav");
-let navToggle = document.querySelector(".main-nav__toggle");
+let mainNav = document.querySelector('.main-nav');
+let navButton = document.querySelector('.main-nav__toggle');
 
-nav.classList.remove(".main-nav--nojs");
+mainNav.classList.remove("main-nav--nojs");
 
-navToggle.addEventListener("click", function () {
-  nav.classList.toggle(".main-nav--closed");
-  if (nav.classList.contains(".main-nav--closed")) {
-    nav.classList.remove(".main-nav--closed");
-    nav.classList.add(".main-nav--opened");
-  } else {
-    nav.classList.add(".main-nav--closed");
-    nav.classList.remove(".main-nav--opened");
-  }
-});
+navButton.onclick = function() {
+  mainNav.classList.toggle('main-nav--opened');
+  mainNav.classList.toggle('main-nav--closed');
+};
